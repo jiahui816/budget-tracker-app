c6f7b1d93135f1bbd08d4dbc750178cd7d26c207
Jia Hui Liou <jiahui816@gmail.com>
2021-03-13 14:40:12 +1100

    initialised server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World - Budget Tracker App',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
