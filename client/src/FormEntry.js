1139643937b8ce5e8febd9849847882dddf3ae52
Jia Hui Liou <jiahui816@gmail.com>
2021-03-15 13:38:27 +1100

    add loading

--- client/src/FormEntry.js
+++ client/src/FormEntry.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBudgetEntry, createLogEntry } from './API';
 import { Form, Button } from 'semantic-ui-react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
 import './FormEntry.css';
 const FormEntry = () => {
   const { register, handleSubmit } = useForm();
-
+  const [loading, setLoading] = useState(false);
   const submit = (data, e) => {
+    setLoading(true);
     createLogEntry(data);
     setTimeout(() => {
+      setLoading(false);
       e.target.reset();
       window.location.reload();
     }, 1500);
   };
 
   const submitBudget = (data, e) => {
+    setLoading(true);
     createBudgetEntry(data);
     setTimeout(() => {
+      setLoading(false);
       e.target.reset();
       window.location.reload();
     }, 1500);
@@ -25,36 +30,41 @@ const FormEntry = () => {
 
   return (
     <div className='form'>
-      <Form onSubmit={handleSubmit(submit)}>
-        <Form.Field>
-          <label>Name</label>
-          <input placeholder='Item' name='item_name' ref={register} />
-        </Form.Field>
-        <Form.Field>
-          <label>Cost</label>
-          <input placeholder='💰' name='item_cost' ref={register} />
-        </Form.Field>
-        <Form.Field>
-          <label>Date</label>
-          <input type='date' name='date' ref={register} />
-        </Form.Field>
-        <Button
-          className='button'
-          type='submit'
-          style={{ backgroundColor: 'lightgreen', color: 'black' }}
-        >
-          Add to Database ➕
-        </Button>
-
-        <Link to='/viewAll'>
+      {loading ? (
+        <ClipLoader size={100} color={'black'} loading={loading} />
+      ) : (
+        <Form onSubmit={handleSubmit(submit)}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Item' name='item_name' ref={register} />
+          </Form.Field>
+          <Form.Field>
+            <label>Cost</label>
+            <input placeholder='💰' name='item_cost' ref={register} />
+          </Form.Field>
+          <Form.Field>
+            <label>Date</label>
+            <input type='date' name='date' ref={register} />
+          </Form.Field>
           <Button
             className='button'
-            style={{ backgroundColor: 'coral', color: 'black' }}
+            type='submit'
+            style={{ backgroundColor: 'lightgreen', color: 'black' }}
           >
-            View All Logs 📝
+            Add to Database ➕
           </Button>
-        </Link>
-      </Form>
+
+          <Link to='/viewAll'>
+            <Button
+              className='button'
+              style={{ backgroundColor: 'coral', color: 'black' }}
+            >
+              View All Logs 📝
+            </Button>
+          </Link>
+        </Form>
+      )}
+
       <Form onSubmit={handleSubmit(submitBudget)}>
         <Form.Field>
           <label>Monthly Budget </label>
